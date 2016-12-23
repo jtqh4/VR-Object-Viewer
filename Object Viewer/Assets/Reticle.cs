0ef3a543725df590a1fea4587ab56a4057f3d78f
Jay Taft <Jay Taft>
2016-12-23 09:05:30 -0600

    Create Reticle and Rotate Cube
    This change creates a HUD reticle for the user to help see where
    the middle of the camera is facing. It also rotates the cube
    towards the direction of the reticle.

---
+++ Object Viewer/Assets/Reticle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour {
+    public Camera CameraFacing;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(CameraFacing.transform.position);
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+        transform.position = CameraFacing.transform.position +
+            CameraFacing.transform.rotation * Vector3.forward * 2;
+	}
+}
---
+++ Object Viewer/Assets/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    public Camera CameraFacing;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float CameraDistance = transform.position.z - CameraFacing.transform.position.z;
+        Vector3 CameraSightVector = CameraFacing.transform.position +
+            CameraFacing.transform.rotation * Vector3.forward * CameraDistance;
+        
+        transform.Rotate(CameraSightVector.y - transform.position.y, transform.position.x - CameraSightVector.x, transform.position.z - CameraSightVector.z, Space.World);
+        
+    }
+}
